093681ec7f448f11850da23728bbe081f8b49259
egorl <egor.liap@icloud.com>
2024-12-03 13:21:54 +0300

    Data types and operators solved

--- Data types and operators/7 task/Main.java
+++ Data types and operators/7 task/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int a = 1;
+        @SuppressWarnings("unused")
         int i = (a == 1) ? 1 : (a == 2) ? 2 : 3; 
     }
 }
